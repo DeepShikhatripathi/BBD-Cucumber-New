39e083c078b8c13730facac1548b7c1b08a56a44
SHIKHAPANDEY <tripathi.shikha29july@gmail.com>
2020-10-26 16:27:11 +0530

    add data driven example

--- src/test/java/stepDefinition/TestRunner.java
+++ src/test/java/stepDefinition/TestRunner.java
@@ -8,13 +8,17 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-		features = "src/test/resources/features" ,
+		features = "src/test/resources/features",
 		glue = "stepDefinition",
 	    plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:target/cucumber-reports/report.html"},
-		monochrome = true
-         )
+		monochrome = true ,
+		tags = {"@datadriven"}
+        )
 
 
-public class TestRunner {
-
-}
+    public class TestRunner {
+	
+	
+	
+	
+    }
